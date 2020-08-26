a3a99f7f2d1f27f5d95617fffc95c81429100bbb
Kenderson Tripaldi <kenderson.tripaldi@ors.ai>
2020-08-26 17:17:34 +0200

    Shorten path names in tests

--- src/services/__tests__/store.spec.ts
+++ src/services/__tests__/store.spec.ts
@@ -31,7 +31,7 @@ const randomValues = [
   [1, 2, 3],
   false,
   null,
-  ({ get }: SetterHelpers<number>) => (get('baseTraitPath1') as number) * 2
+  ({ get }: SetterHelpers<number>) => (get('basePath1') as number) * 2
 ]
 
 describe('The Store', () => {
@@ -63,9 +63,9 @@ describe('The Store', () => {
   it(`should throw an error, if you try to set or get a Trait without creating the store`, () => {
     const testValue = 'testValue'
     expect(() =>
-      storeActions.setTrait<typeof testValue>('testTraitPath', testValue)
+      storeActions.setTrait<typeof testValue>('testPath', testValue)
     ).toThrow(MESSAGES.ERRORS.NO_STORE_FOUND)
-    expect(() => storeActions.getTrait('testTraitPath')).toThrow(
+    expect(() => storeActions.getTrait('testPath')).toThrow(
       MESSAGES.ERRORS.NO_STORE_FOUND
     )
   })
@@ -104,16 +104,16 @@ describe('The Store', () => {
     storeActions.create()
     // With an immutable value
     const testValue1 = 'testValue1'
-    storeActions.setTrait<typeof testValue1>('testTraitPath1', testValue1)
+    storeActions.setTrait<typeof testValue1>('testPath1', testValue1)
     const testValue2 = 'testValue2'
-    storeActions.setTrait<typeof testValue2>('testTraitPath1', testValue2)
-    expect(storeActions.getTrait('testTraitPath1')).toEqual(testValue2)
+    storeActions.setTrait<typeof testValue2>('testPath1', testValue2)
+    expect(storeActions.getTrait('testPath1')).toEqual(testValue2)
     // With a mutable value
     const testValue3 = { key1: { key2: 'testValue3' } }
-    storeActions.setTrait<typeof testValue3>('testTraitPath2', testValue3)
+    storeActions.setTrait<typeof testValue3>('testPath2', testValue3)
     const testValue4 = { key1: { key2: 'testValue4' } }
-    storeActions.setTrait<typeof testValue4>('testTraitPath2', testValue4)
-    expect(storeActions.getTrait('testTraitPath2')).toEqual(testValue4)
+    storeActions.setTrait<typeof testValue4>('testPath2', testValue4)
+    expect(storeActions.getTrait('testPath2')).toEqual(testValue4)
   })
 
   it(`should log when a Trait is updated, if you have the debug enabled`, () => {
@@ -121,20 +121,20 @@ describe('The Store', () => {
     storeActions.create({ debug: true })
     // With an immutable value
     const testValue1 = 'testValue1'
-    storeActions.setTrait<typeof testValue1>('testTraitPath1', testValue1)
+    storeActions.setTrait<typeof testValue1>('testPath1', testValue1)
     const testValue2 = 'testValue2'
-    storeActions.setTrait<typeof testValue2>('testTraitPath1', testValue2)
+    storeActions.setTrait<typeof testValue2>('testPath1', testValue2)
     expect(consoleSpy).toHaveBeenCalledWith(
-      format(MESSAGES.LOGS.TRAIT_UPDATED, 'testTraitPath1'),
+      format(MESSAGES.LOGS.TRAIT_UPDATED, 'testPath1'),
       testValue2
     )
     // With a mutable value
     const testValue3 = { key1: { key2: 'testValue3' } }
-    storeActions.setTrait<typeof testValue3>('testTraitPath2', testValue3)
+    storeActions.setTrait<typeof testValue3>('testPath2', testValue3)
     const testValue4 = { key1: { key2: 'testValue4' } }
-    storeActions.setTrait<typeof testValue4>('testTraitPath2', testValue4)
+    storeActions.setTrait<typeof testValue4>('testPath2', testValue4)
     expect(consoleSpy).toHaveBeenCalledWith(
-      format(MESSAGES.LOGS.TRAIT_UPDATED, 'testTraitPath2'),
+      format(MESSAGES.LOGS.TRAIT_UPDATED, 'testPath2'),
       testValue4
     )
     consoleSpy.mockClear()
@@ -142,20 +142,17 @@ describe('The Store', () => {
 
   it(`should do nothing, if you try to update an existing Trait with the same value`, () => {
     storeActions.create()
-    storeActions.setTrait<number>('baseTraitPath1', 1)
+    storeActions.setTrait<number>('basePath1', 1)
     randomValues.forEach((testValue, index) => {
-      storeActions.setTrait<typeof testValue>(
-        `testTraitPath${index}`,
-        testValue
-      )
-      const traitValue1 = storeActions.getTrait(`testTraitPath${index}`)
+      storeActions.setTrait<typeof testValue>(`testPath${index}`, testValue)
+      const traitValue1 = storeActions.getTrait(`testPath${index}`)
       // I need to pass a copy of the same value, so I'll duplicate the array of randomValues and keep the same index
       const testValueCopy = randomValues.slice()[index]
       storeActions.setTrait<typeof testValueCopy>(
-        `testTraitPath${index}`,
+        `testPath${index}`,
         testValueCopy
       )
-      const traitValue2 = storeActions.getTrait(`testTraitPath${index}`)
+      const traitValue2 = storeActions.getTrait(`testPath${index}`)
       expect(traitValue1).toEqual(traitValue2)
     })
   })
@@ -164,16 +161,16 @@ describe('The Store', () => {
     storeActions.create()
     const baseValue = 'testValue'
     const differentTypeValue = false
-    storeActions.setTrait<typeof baseValue>('testTraitPath', baseValue)
+    storeActions.setTrait<typeof baseValue>('testPath', baseValue)
     expect(() => {
       storeActions.setTrait<typeof differentTypeValue>(
-        'testTraitPath',
+        'testPath',
         differentTypeValue
       )
     }).toThrow(
       format(
         MESSAGES.ERRORS.TRAIT_WRONG_TYPE,
-        'testTraitPath',
+        'testPath',
         typeof baseValue,
         typeof differentTypeValue
       )
@@ -183,37 +180,30 @@ describe('The Store', () => {
   it(`should let you set to 'undefined' the value of an existing Trait`, () => {
     storeActions.create()
     const testValue = 'testValue'
-    storeActions.setTrait<typeof testValue>('testTraitPath', testValue)
-    storeActions.setTrait<undefined>('testTraitPath', undefined)
-    expect(storeActions.getTrait('testTraitPath')).toEqual(undefined)
+    storeActions.setTrait<typeof testValue>('testPath', testValue)
+    storeActions.setTrait<undefined>('testPath', undefined)
+    expect(storeActions.getTrait('testPath')).toEqual(undefined)
   })
 
   it(`should update the Trait value, if TraitValue is an updater`, () => {
     storeActions.create()
     const testValue = 'testValue'
-    storeActions.setTrait<typeof testValue>('testTraitPath', testValue)
+    storeActions.setTrait<typeof testValue>('testPath', testValue)
     storeActions.setTrait<string>(
-      'testTraitPath',
+      'testPath',
       ({ value }: SetterHelpers<string>) => value.toUpperCase()
     )
-    expect(storeActions.getTrait('testTraitPath')).toEqual(
-      testValue.toUpperCase()
-    )
+    expect(storeActions.getTrait('testPath')).toEqual(testValue.toUpperCase())
   })
 
   it(`should let you set an alternative path separator`, () => {
     storeActions.create({ pathSeparator: '>' })
     const testValue = 'testValue'
-    storeActions.setTrait<typeof testValue>(
-      'testTraitPathSeparator1>testTraitPathSeparator2',
-      testValue
-    )
-    expect(storeActions.getTrait('testTraitPathSeparator1')).toEqual({
-      testTraitPathSeparator2: testValue
+    storeActions.setTrait<typeof testValue>('testPath1>testPath2', testValue)
+    expect(storeActions.getTrait('testPath1')).toEqual({
+      testPath2: testValue
     })
-    expect(
-      storeActions.getTrait('testTraitPathSeparator1>testTraitPathSeparator2')
-    ).toEqual(testValue)
+    expect(storeActions.getTrait('testPath1>testPath2')).toEqual(testValue)
   })
 
   it(`should create a selector and dispatch its updates, if TraitValue is a callback that refers another Trait`, () => {
@@ -222,47 +212,33 @@ describe('The Store', () => {
     const testValue1 = 5
     const testValue2 = 10
     const multiplier = 2
-    storeActions.setTrait<typeof testValue1>('baseTraitPath1', testValue1)
-    storeActions.setTrait<typeof testValue1>(
-      'baseTraitPath2.baseTraitPath3',
-      testValue1
-    )
+    storeActions.setTrait<typeof testValue1>('basePath1', testValue1)
+    storeActions.setTrait<typeof testValue1>('basePath2.basePath3', testValue1)
 
-    // We'll create two selectors, one based on Trait `baseTraitPath1` and another one based on Trait `baseTraitPath2//baseTraitPath`
-    // Selector based on Trait `baseTraitPath1`
+    // We'll create two selectors, one based on Trait `basePath1` and another one based on Trait `basePath2//basePath`
+    // Selector based on Trait `basePath1`
     storeActions.setTrait<number>(
-      'testTraitPath1',
+      'testPath1',
       ({ get }: SetterHelpers<number>) =>
-        (get('baseTraitPath1') as number) * multiplier
+        (get('basePath1') as number) * multiplier
     )
-    // Since `testTraitPath1` is a selector, it is expected to return its value based on the one of `baseTraitPath1`
-    expect(storeActions.getTrait('testTraitPath1')).toEqual(
-      testValue1 * multiplier
-    )
-    // If `baseTraitPath1` value changes, `testTraitPath1` is expected to update accordingly
-    storeActions.setTrait<typeof testValue2>('baseTraitPath1', testValue2)
-    expect(storeActions.getTrait('testTraitPath1')).toEqual(
-      testValue2 * multiplier
-    )
+    // Since `testPath1` is a selector, it is expected to return its value based on the one of `basePath1`
+    expect(storeActions.getTrait('testPath1')).toEqual(testValue1 * multiplier)
+    // If `basePath1` value changes, `testPath1` is expected to update accordingly
+    storeActions.setTrait<typeof testValue2>('basePath1', testValue2)
+    expect(storeActions.getTrait('testPath1')).toEqual(testValue2 * multiplier)
 
-    // Selector based on Trait `baseTraitPath2//baseTraitPath`
+    // Selector based on Trait `basePath2//basePath`
     storeActions.setTrait<number>(
-      'testTraitPath2',
+      'testPath2',
       ({ get }: SetterHelpers<number>) =>
-        (get('baseTraitPath2.baseTraitPath3') as number) * multiplier
+        (get('basePath2.basePath3') as number) * multiplier
     )
-    // Since `testTraitPath2` is a selector, it is expected to return its value based on the one of `baseTraitPath2//baseTraitPath`
-    expect(storeActions.getTrait('testTraitPath2')).toEqual(
-      testValue1 * multiplier
-    )
-    // If `baseTraitPath1` value changes, `testTraitPath2` is expected to update accordingly
-    storeActions.setTrait<typeof testValue2>(
-      'baseTraitPath2.baseTraitPath3',
-      testValue2
-    )
-    expect(storeActions.getTrait('testTraitPath2')).toEqual(
-      testValue2 * multiplier
-    )
+    // Since `testPath2` is a selector, it is expected to return its value based on the one of `basePath2//basePath`
+    expect(storeActions.getTrait('testPath2')).toEqual(testValue1 * multiplier)
+    // If `basePath1` value changes, `testPath2` is expected to update accordingly
+    storeActions.setTrait<typeof testValue2>('basePath2.basePath3', testValue2)
+    expect(storeActions.getTrait('testPath2')).toEqual(testValue2 * multiplier)
   })
 
   it(`should log when a selector is updated, if you have the debug enabled`, () => {
@@ -272,20 +248,16 @@ describe('The Store', () => {
     const testValue1 = 5
     const testValue2 = 10
     const multiplier = 2
-    storeActions.setTrait<typeof testValue1>('baseTraitPath', testValue1)
+    storeActions.setTrait<typeof testValue1>('basePath', testValue1)
     storeActions.setTrait<number>(
-      'selectorTraitPath',
+      'selectorPath',
       ({ get }: SetterHelpers<number>) =>
-        (get('baseTraitPath') as number) * multiplier
+        (get('basePath') as number) * multiplier
     )
-    storeActions.subscribeToTrait('selectorTraitPath', () => {})
-    storeActions.setTrait<typeof testValue2>('baseTraitPath', testValue2)
+    storeActions.subscribeToTrait('selectorPath', () => {})
+    storeActions.setTrait<typeof testValue2>('basePath', testValue2)
     expect(consoleSpy).toHaveBeenCalledWith(
-      format(
-        MESSAGES.LOGS.SELECTOR_UPDATED,
-        'selectorTraitPath',
-        'baseTraitPath'
-      ),
+      format(MESSAGES.LOGS.SELECTOR_UPDATED, 'selectorPath', 'basePath'),
       testValue2 * multiplier
     )
     consoleSpy.mockClear()
@@ -295,16 +267,16 @@ describe('The Store', () => {
     storeActions.create()
     expect(() =>
       storeActions.setTrait<unknown>(
-        'wrongSelector',
-        (helpers: SetterHelpers<unknown>) => helpers.get('nonExisitingTrait')
+        'testWrongSelector',
+        ({ get }: SetterHelpers<unknown>) => get('testNoPath')
       )
-    ).toThrow(format(MESSAGES.ERRORS.TRAIT_DOES_NOT_EXIST, 'nonExisitingTrait'))
+    ).toThrow(format(MESSAGES.ERRORS.TRAIT_DOES_NOT_EXIST, 'testNoPath'))
   })
 
   it(`should throw an error, if you try to subscribe to a Trait with with no path`, () => {
     storeActions.create()
     const testValue = 'testValue'
-    storeActions.setTrait<typeof testValue>('testTraitPath', 'testValue')
+    storeActions.setTrait<typeof testValue>('testPath', 'testValue')
     expect(() => {
       // @ts-ignore
       storeActions.subscribeToTrait<unknown>(() => {})
@@ -322,13 +294,13 @@ describe('The Store', () => {
     storeActions.create()
     expect(() => {
       // @ts-ignore
-      storeActions.subscribeToTrait<unknown>('testTraitPath')
+      storeActions.subscribeToTrait<unknown>('testPath')
     }).toThrow(MESSAGES.ERRORS.SUBSCRIPTION_NO_CALLBACK)
   })
 
   it(`should throw an error, if you try to subscribe to a Trait without creating the store`, () => {
     expect(() =>
-      storeActions.subscribeToTrait<unknown>('testTraitPath', () => {})
+      storeActions.subscribeToTrait<unknown>('testPath', () => {})
     ).toThrow(MESSAGES.ERRORS.NO_STORE_FOUND)
   })
 
@@ -336,27 +308,21 @@ describe('The Store', () => {
     storeActions.create()
     // With an immutable value
     const testValue1 = 'testValue'
-    storeActions.setTrait<typeof testValue1>('testTraitPath1', testValue1)
+    storeActions.setTrait<typeof testValue1>('testPath1', testValue1)
     let testState1
     const callback1 = (value: unknown) => (testState1 = value)
-    storeActions.subscribeToTrait<typeof testValue1>(
-      'testTraitPath1',
-      callback1
-    )
+    storeActions.subscribeToTrait<typeof testValue1>('testPath1', callback1)
     const testValue2 = 'testValue2'
-    storeActions.setTrait<typeof testValue2>('testTraitPath1', testValue2)
+    storeActions.setTrait<typeof testValue2>('testPath1', testValue2)
     expect(testState1).toEqual(testValue2)
     // With a mutable value
     const testValue3 = { key1: { key2: 'testValue3' } }
-    storeActions.setTrait<typeof testValue3>('testTraitPath2', testValue3)
+    storeActions.setTrait<typeof testValue3>('testPath2', testValue3)
     let testState2
     const callback2 = (value: unknown) => (testState2 = value)
-    storeActions.subscribeToTrait<typeof testValue3>(
-      'testTraitPath2',
-      callback2
-    )
+    storeActions.subscribeToTrait<typeof testValue3>('testPath2', callback2)
     const testValue4 = { key1: { key2: 'testValue4' } }
-    storeActions.setTrait<typeof testValue4>('testTraitPath2', testValue4)
+    storeActions.setTrait<typeof testValue4>('testPath2', testValue4)
     expect(testState2).toEqual(testValue4)
   })
 
@@ -364,9 +330,9 @@ describe('The Store', () => {
     storeActions.create()
     let testState
     const callback = (value: unknown) => (testState = value)
-    storeActions.subscribeToTrait<unknown>('testTraitPath', callback)
+    storeActions.subscribeToTrait<unknown>('testPath', callback)
     const testValue = 'testValue2'
-    storeActions.setTrait<typeof testValue>('testTraitPath', testValue)
+    storeActions.setTrait<typeof testValue>('testPath', testValue)
     expect(testState).toEqual(testValue)
   })
 
@@ -376,17 +342,17 @@ describe('The Store', () => {
     storeActions.create({ storageService })
     // When a Trait is set or updated, it should call the storage service to save the new value
     const testValue1 = 'testValue1'
-    storeActions.setTrait<typeof testValue1>('testTraitPath', testValue1)
+    storeActions.setTrait<typeof testValue1>('testPath', testValue1)
     expect(storageService.set).toHaveBeenCalled()
     const testValue2 = 'testValue2'
-    storeActions.setTrait<typeof testValue2>('testTraitPath', testValue2)
+    storeActions.setTrait<typeof testValue2>('testPath', testValue2)
     expect(storageService.set).toHaveBeenCalled()
     // If a Trait doesn't exist, it should call the storage service to check if there's saved version
-    const nonExistingTrait = storeActions.getTrait('testNonExistingTrait')
+    const nonExistingTrait = storeActions.getTrait('testNoPath')
     expect(storageService.get).toHaveBeenCalled()
     expect(nonExistingTrait).toEqual(testStoredValue)
     // When a trait is set to undefined, it should call the storage service to clear the value
-    storeActions.setTrait<undefined>('testTraitPath', undefined)
+    storeActions.setTrait<undefined>('testPath', undefined)
     expect(storageService.clear).toHaveBeenCalled()
   })
 
@@ -394,7 +360,7 @@ describe('The Store', () => {
     const storageService = mockStorageService({ get: false })
     // @ts-ignore Here we create a new store with the storage service
     storeActions.create({ storageService })
-    expect(() => storeActions.getTrait('testTraitPath')).toThrow(
+    expect(() => storeActions.getTrait('testPath')).toThrow(
       MESSAGES.ERRORS.STORAGE_MISS_GET
     )
   })
@@ -405,7 +371,7 @@ describe('The Store', () => {
     storeActions.create({ storageService })
     expect(() => {
       const testValue = 'testValue'
-      storeActions.setTrait<typeof testValue>('testTraitPath', testValue)
+      storeActions.setTrait<typeof testValue>('testPath', testValue)
     }).toThrow(MESSAGES.ERRORS.STORAGE_MISS_SET)
   })
 
@@ -414,7 +380,7 @@ describe('The Store', () => {
     // @ts-ignore
     storeActions.create({ storageService })
     expect(() =>
-      storeActions.setTrait<undefined>('testTraitPath', undefined)
+      storeActions.setTrait<undefined>('testPath', undefined)
     ).toThrow(MESSAGES.ERRORS.STORAGE_MISS_CLEAR)
   })
 
@@ -423,9 +389,9 @@ describe('The Store', () => {
     const storageService = mockStorageService()
     // @ts-ignore
     storeActions.create({ storageService, debug: true })
-    storeActions.getTrait('testTraitPath')
+    storeActions.getTrait('testPath')
     expect(consoleSpy).toHaveBeenCalledWith(
-      format(MESSAGES.LOGS.STORAGE_IMPORTED, 'testTraitPath'),
+      format(MESSAGES.LOGS.STORAGE_IMPORTED, 'testPath'),
       testStoredValue
     )
     consoleSpy.mockClear()
@@ -437,9 +403,9 @@ describe('The Store', () => {
     // @ts-ignore
     storeActions.create({ storageService, debug: true })
     const testValue = 'testValue'
-    storeActions.setTrait<typeof testValue>('testTraitPath', testValue)
+    storeActions.setTrait<typeof testValue>('testPath', testValue)
     expect(consoleSpy).toHaveBeenCalledWith(
-      format(MESSAGES.LOGS.STORAGE_SAVED, 'testTraitPath'),
+      format(MESSAGES.LOGS.STORAGE_SAVED, 'testPath'),
       testValue
     )
     consoleSpy.mockClear()
@@ -450,9 +416,9 @@ describe('The Store', () => {
     const storageService = mockStorageService()
     // @ts-ignore
     storeActions.create({ storageService, debug: true })
-    storeActions.setTrait<undefined>('testTraitPath', undefined)
+    storeActions.setTrait<undefined>('testPath', undefined)
     expect(consoleSpy).toHaveBeenCalledWith(
-      format(MESSAGES.LOGS.STORAGE_REMOVED, 'testTraitPath'),
+      format(MESSAGES.LOGS.STORAGE_REMOVED, 'testPath'),
       ''
     )
     consoleSpy.mockClear()
