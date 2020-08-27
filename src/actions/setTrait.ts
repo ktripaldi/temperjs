c130a320aaa2afd88cf3f8e7fe32b55dca037adf
Kenderson Tripaldi <kenderson.tripaldi@ors.ai>
2020-08-27 11:09:50 +0200

    Reformat comments in actions

--- src/actions/setTrait.ts
+++ src/actions/setTrait.ts
@@ -8,8 +8,6 @@ import { SetterHelpers } from '..'
  * @param {T} traitValue - This is the Trait value
  *
  * Traits can be anything. When a Trait is an object, each attribute will become a new Trait that is individually subscribable.
- * When you create a Trait, you can target any level of an object using the dot notation (ex. level1.level2)
- * `setTrait(level1.level2, value)` will create a trait with this value: `{ level1 : { level2 : value }}`
  */
 function setTrait<T>(
   path: string,
