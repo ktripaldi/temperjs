b3cd1d22625764975ad0fcb30261f01c3565eee8
Kenderson Tripaldi <kenderson.tripaldi@ors.ai>
2020-08-27 11:08:12 +0200

    Export `Loadable` and `SubscriptionOptions` interfaces

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { default as getTrait } from './actions/getTrait'
 export { default as setTrait } from './actions/setTrait'
 export { default as useTrait } from './hooks/useTrait'
 export { default as useTraitValue } from './hooks/useTraitValue'
+export { Loadable, SubscriptionOptions } from './utils/rxSubject'
 export {
   Subscription,
   SetterHelpers,
