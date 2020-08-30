565ad16151a22f315faf8fb8fb6188d4e40b7f5d
Kenderson Tripaldi <kenderson.tripaldi@ors.ai>
2020-08-30 23:10:10 +0200

    Update `useTraitValue` tests

--- src/hooks/__tests__/useTraitValue.spec.ts
+++ src/hooks/__tests__/useTraitValue.spec.ts
@@ -12,14 +12,18 @@ describe('The hook `useTraitValue`', () => {
 
   it(`should return the current value of a Trait, at the first usage`, () => {
     randomValues.forEach((testValue, index) => {
-      storeActions.setTrait<typeof testValue>(`testPath${index}`, testValue)
+      act(() => {
+        storeActions.setTrait<typeof testValue>(`testPath${index}`, testValue)
+      })
       const { result } = renderHook(() => useTraitValue(`testPath${index}`))
       expect(result.current).toEqual(storeActions.getTrait(`testPath${index}`))
     })
   })
 
   it(`should return the updated value of a Trait, when the Trait value changes`, () => {
-    storeActions.setTrait('testPath', 'testValue')
+    act(() => {
+      storeActions.setTrait('testPath', 'testValue')
+    })
     const testValue = 'testValue2'
     const { result } = renderHook(() => useTraitValue('testPath'))
     act(() => {
@@ -30,7 +34,9 @@ describe('The hook `useTraitValue`', () => {
 
   it(`should return the updated value of a Trait (shallow copy), when a sub Trait value changes`, () => {
     const testValue = { key1: { key2: 'testValue' } }
-    storeActions.setTrait<typeof testValue>('testPath', testValue)
+    act(() => {
+      storeActions.setTrait<typeof testValue>('testPath', testValue)
+    })
     const testValue2 = 'testValue2'
     const { result } = renderHook(() => useTraitValue('testPath'))
     act(() => {
@@ -43,12 +49,14 @@ describe('The hook `useTraitValue`', () => {
   it(`should return the updated value of a Trait, if the Trait is a selector and the base Trait value changes`, () => {
     const testValue1 = 'testValue1'
     const testValue2 = 'testValue2'
-    storeActions.setTrait<typeof testValue1>('testPath1', testValue1)
-    storeActions.setTrait<string>(
-      'testPath2',
-      ({ get }: SetterHelpers<string>) =>
-        (get('testPath1') as string).toUpperCase()
-    )
+    act(() => {
+      storeActions.setTrait<typeof testValue1>('testPath1', testValue1)
+      storeActions.setTrait<string>(
+        'testPath2',
+        ({ get }: SetterHelpers<string>) =>
+          (get('testPath1') as string).toUpperCase()
+      )
+    })
     const { result } = renderHook(() => useTraitValue('testPath2'))
     act(() => {
       storeActions.setTrait('testPath1', testValue2)
@@ -60,51 +68,62 @@ describe('The hook `useTraitValue`', () => {
     storeActions.create()
     const testDefaultValue = 'testValue'
     // If the Trait is undefined, we should get the default value
-    const { result: result1 } = renderHook(() =>
-      useTraitValue('testPath1', { default: testDefaultValue })
-    )
-    expect(result1.current).toEqual(testDefaultValue)
+    let hook1: any
+    act(() => {
+      hook1 = renderHook(() =>
+        useTraitValue('testPath1', { default: testDefaultValue })
+      )
+    })
+    expect(hook1.result.current).toEqual(testDefaultValue)
     // In the case we also want the default value to be become the actual value of the Trait
     expect(storeActions.getTrait('testPath1')).toEqual(testDefaultValue)
 
     // If the Trait already exists and has a value, we should get the actual value
     const testValue = 'testValue'
-    storeActions.setTrait('testPath2', testValue)
-    const { result: result2 } = renderHook(() =>
-      useTraitValue('testPath2', { default: testDefaultValue })
-    )
-    expect(result2.current).toEqual(testValue)
+    let hook2: any
+    act(() => {
+      storeActions.setTrait('testPath2', testValue)
+      hook2 = renderHook(() =>
+        useTraitValue('testPath2', { default: testDefaultValue })
+      )
+    })
+    expect(hook2.result.current).toEqual(testValue)
     // In the case we don't want the actual value of the Trait to change
     expect(storeActions.getTrait('testPath2')).toEqual(testValue)
   })
 
   it(`should return a Loadable Trait, if you subscribe with the option 'loadable' set to true`, () => {
     storeActions.create()
-    const testPromiseValue = new Promise(resolve => {
-      resolve('testValue')
+    // This is needed to prevent `testPromiseValue` from unwrapping
+    var deferred = []
+    const testPromiseValue = new Promise((resolve, reject) => {
+      deferred.push({ resolve: resolve, reject: reject })
     })
-    storeActions.setTrait('testPath', testPromiseValue)
-    const { result } = renderHook(() =>
-      useTraitValue('testPath', { loadable: true })
-    )
-    expect(result.current).toEqual({
+    let hook: any
+    act(() => {
+      storeActions.setTrait('testPath', testPromiseValue)
+      hook = renderHook(() => useTraitValue('testPath', { loadable: true }))
+    })
+    expect(hook.result.current).toEqual({
       state: LoadableState.LOADING,
       value: testPromiseValue
     })
   })
 
-  it(`should return a resolved value, if the Loadable Trait unwraps correctly`, done => {
+  it(`should return a resolved value, if the Loadable Trait unwraps correctly`, async done => {
     storeActions.create()
     const testValue = 'testValue'
     const testPromiseValue = new Promise(resolve => {
       resolve(testValue)
     })
-    storeActions.setTrait('testPath', testPromiseValue)
-    const { result } = renderHook(() =>
-      useTraitValue('testPath', { loadable: true })
-    )
+    let hook: any
+    await act(async () => {
+      storeActions.setTrait('testPath', testPromiseValue)
+      hook = renderHook(() => useTraitValue('testPath', { loadable: true }))
+      await hook.waitForNextUpdate()
+    })
     setTimeout(() => {
-      expect(result.current).toEqual({
+      expect(hook.result.current).toEqual({
         state: LoadableState.HAS_VALUE,
         value: testValue
       })
@@ -112,18 +131,20 @@ describe('The hook `useTraitValue`', () => {
     }, 100)
   })
 
-  it(`should return an error, if the Loadable Trait fails to unwrap`, done => {
+  it(`should return an error, if the Loadable Trait fails to unwrap`, async done => {
     storeActions.create()
     const testError = Error('Promise has been rejected')
     const testPromiseValue = new Promise((_, reject) => {
       reject(testError)
     })
-    storeActions.setTrait('testPath', testPromiseValue)
-    const { result } = renderHook(() =>
-      useTraitValue('testPath', { loadable: true })
-    )
+    let hook: any
+    await act(async () => {
+      storeActions.setTrait('testPath', testPromiseValue)
+      hook = renderHook(() => useTraitValue('testPath', { loadable: true }))
+      await hook.waitForNextUpdate()
+    })
     setTimeout(() => {
-      expect(result.current).toEqual({
+      expect(hook.result.current).toEqual({
         state: LoadableState.HAS_ERROR,
         value: testError
       })
